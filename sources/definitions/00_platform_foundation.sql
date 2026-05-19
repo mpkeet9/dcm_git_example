d089131471fb9a27d2871aa9b60aab6e9389c320
Max Keeter <max.keeter@snowflake.com>
2026-05-19 11:37:40 -0600

    rehash

--- sources/definitions/00_platform_foundation.sql
+++ sources/definitions/00_platform_foundation.sql
@@ -2,19 +2,9 @@ DEFINE WAREHOUSE {{ warehouse_name }}
   WITH
     warehouse_size = '{{ warehouse_size }}'
     auto_suspend = 60
-    auto_resume = TRUE
-    initially_suspended = TRUE;
+    auto_resume = TRUE;
 
 DEFINE DATABASE {{ data_db }};
 
 DEFINE SCHEMA {{ data_db }}.{{ data_schema }};
 DEFINE SCHEMA {{ data_db }}.{{ analytics_schema }};
-
-GRANT USAGE ON WAREHOUSE {{ warehouse_name }} TO ROLE {{ deployer_role }};
-GRANT USAGE ON WAREHOUSE {{ warehouse_name }} TO ROLE {{ reader_role }};
-
-GRANT USAGE ON DATABASE {{ data_db }} TO ROLE {{ deployer_role }};
-GRANT USAGE ON DATABASE {{ data_db }} TO ROLE {{ reader_role }};
-
-GRANT USAGE ON SCHEMA {{ data_db }}.{{ data_schema }} TO ROLE {{ deployer_role }};
-GRANT USAGE ON SCHEMA {{ data_db }}.{{ data_schema }} TO ROLE {{ reader_role }};
--- sources/definitions/20_grants.sql
+++ sources/definitions/20_grants.sql
@@ -1,10 +1,9 @@
+GRANT USAGE ON WAREHOUSE {{ warehouse_name }} TO ROLE {{ reader_role }};
+
+GRANT USAGE ON DATABASE {{ data_db }} TO ROLE {{ reader_role }};
+GRANT USAGE ON SCHEMA {{ data_db }}.{{ data_schema }} TO ROLE {{ reader_role }};
+GRANT USAGE ON SCHEMA {{ data_db }}.{{ analytics_schema }} TO ROLE {{ reader_role }};
+
 GRANT SELECT ON TABLE {{ data_db }}.{{ data_schema }}.CUSTOMERS TO ROLE {{ reader_role }};
 GRANT SELECT ON TABLE {{ data_db }}.{{ data_schema }}.ORDERS TO ROLE {{ reader_role }};
 GRANT SELECT ON VIEW {{ data_db }}.{{ analytics_schema }}.DAILY_ORDER_SUMMARY TO ROLE {{ reader_role }};
-
-GRANT USAGE ON SCHEMA {{ data_db }}.{{ analytics_schema }} TO ROLE {{ reader_role }};
-
--- The deployer role remains the managing role for DCM-owned objects in this sample.
-GRANT OWNERSHIP ON TABLE {{ data_db }}.{{ data_schema }}.CUSTOMERS TO ROLE {{ deployer_role }} COPY CURRENT GRANTS;
-GRANT OWNERSHIP ON TABLE {{ data_db }}.{{ data_schema }}.ORDERS TO ROLE {{ deployer_role }} COPY CURRENT GRANTS;
-GRANT OWNERSHIP ON VIEW {{ data_db }}.{{ analytics_schema }}.DAILY_ORDER_SUMMARY TO ROLE {{ deployer_role }} COPY CURRENT GRANTS;
